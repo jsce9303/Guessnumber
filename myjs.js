dbab0cf7658dd9b93c1edddb507814d3fc7ec607
DESKTOP-U4O9AAT\MIT-302-02 <jsce9303@naver.com>
2022-05-10 17:14:31 +0900

    1차 완성ㅅ

---
+++ myjs.js
@@ -0,0 +1,53 @@
+let secretnumber= Math.floor(Math.random() * 100) + 1;
+let score=20;
+
+
+// 정답이 될 수를 랜덤숫자로 가져옴
+document.querySelector('.number').textContent=secretnumber;
+// check버튼 클릭시 이벤트 발동
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+
+    console.log(guess, typeof guess);
+    document.querySelector('.again'); 
+    //아무것도 입력 안할때
+    if(!guess){
+        document.querySelector('.message').textContent= '숫자를 입력해주세요!';
+        
+    }
+    // 숫자를 맞췄을 경우
+    else if(guess === secretnumber){
+        document.querySelector('.message').textContent= '정답입니다!';
+        document.querySelector('.number').textContent=secretnumber;
+
+        document.querySelector('.container').style.backgroundColor="#094699";
+        document.querySelector('.box').textContent=guess;
+
+    // 숫자를 못맞출경우
+    }else if(guess !==secretnumber){
+        if(score >1){
+            document.querySelector('.message').textContent=guess >secretnumber ?'높습니다!':'낮습니다!';
+            score--; //남은 횟수를 1씩 차감한다.
+            document.querySelector('.score').textContent=score;
+        }else{ //횟수가 1 미만일 경우
+            document.querySelector('.message').textContent='와우 다 꼴았습니다!';
+            document.querySelector('.score').textContent=0;
+        }
+    }
+});
+// again을 누를시 이벤트 발생
+document.querySelector('.retry').addEventListener('click',function(){
+    score =20;
+    secretnumber=Math.trunc(Math.random()*20)+1;
+    //횟수 리필
+    document.querySelector('.message').textContent ='숫자 비교중....';
+    //선언된 score = 20 출력
+    document.querySelector('.score').textContent= score;
+    
+    document.querySelector('.box').textContent='❔';
+
+    document.querySelector('.guess').value='';
+    
+    document.querySelector('.container').style.backgroundColor='#094699';
+    
+});
